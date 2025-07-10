3fa4c8f5d3ccac7b0b88cead1939d6055ccf075b
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:03:46 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Rock24_1.0" `
+  -ModName "Rock 24 1.0" `
+  -ModFolder "Rock 24" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/rock24/Rock-24-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "8574334fcd681fed0ef42634c62ac294a3cae0b2f5952acf4db0d0994c3bdf1b" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
